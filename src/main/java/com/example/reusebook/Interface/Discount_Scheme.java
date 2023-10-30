a667558e71553c658a0ec0bead7eeba939dc6025
rajnshah11 <129009738+rajnshah11@users.noreply.github.com>
2023-10-30 17:34:41 -0400

    Add files via upload

---
+++ src/main/java/com/example/reusebook/Interface/Discount_Scheme.java
@@ -0,0 +1,15 @@
+package com.example.reusebook.Interface;
+
+/**
+ * An interface for discount strategies applied to transactions.
+ */
+public interface Discount_Scheme {
+    /**
+     * Get a discount description for a given price and the number of transactions.
+     *
+     * @param price           The original price of a transaction.
+     * @param noOfTransaction The number of transactions performed.
+     * @return A string describing the discount applied.
+     */
+    String getDiscountDetails(int price, int numofTransaction);
+}
